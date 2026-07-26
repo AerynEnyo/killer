911d3e89e322b77e2fb1dbd6bbc47730d597f22b
AerynEnyo <142051937+AerynEnyo@users.noreply.github.com>
2026-07-26 18:42:17 -0230

    Update script.js

--- script.js
+++ script.js
@@ -31,16 +31,6 @@ let suspects = [];
 let killer = null;
 let generatedClues = [];
 
-const guessInput =
-    document.getElementById("guessInput");
-
-const guessButton =
-    document.getElementById("guessButton");
-
-const guessResult =
-    document.getElementById("guessResult");
-
-
 /* =========================================================
    START THE GAME
 ========================================================= */
